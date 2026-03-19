d8af77c472b2af998621a7f06f75b6f2fafea560
zw-makes <zwmakes@gmail.com>
2026-03-20 01:05:22 +0530

    fix: set PRODUCT_NAME and resolve Info.plist duplicate output

--- ios/App/configure_widget.rb
+++ ios/App/configure_widget.rb
@@ -23,15 +23,18 @@ widget_target.product_name = target_name
 widget_group = project.main_group.find_subpath('WidgetExtension', true)
 widget_group.set_source_tree('<group>')
 
-# Add files to the group and target
-file_names = ['WidgetExtension.swift', 'WidgetExtensionBundle.swift', 'Info.plist']
+# Add files to the group and target (exclude Info.plist from build phases)
+file_names = ['WidgetExtension.swift', 'WidgetExtensionBundle.swift']
 file_names.each do |file_name|
   file_ref = widget_group.new_file(File.join('WidgetExtension', file_name))
   widget_target.add_file_references([file_ref])
 end
+# Keep Info.plist reference in the group but not in build phases
+widget_group.new_file(File.join('WidgetExtension', 'Info.plist'))
 
 # 3. Build settings
 widget_target.build_configurations.each do |config|
+  config.build_settings['PRODUCT_NAME'] = target_name
   config.build_settings['PRODUCT_BUNDLE_IDENTIFIER'] = widget_bundle_id
   config.build_settings['INFOPLIST_FILE'] = 'WidgetExtension/Info.plist'
   config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.0'
